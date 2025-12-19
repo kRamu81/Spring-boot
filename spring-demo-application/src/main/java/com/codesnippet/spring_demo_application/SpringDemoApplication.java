1ce76252cbc1bb43124d383e9e08efc6dd2fbd56
kRamu81 <kanamramu18@gmail.com>
2025-12-19 19:52:14 +0530

    update

---
+++ spring-demo-application/src/main/java/com/codesnippet/spring_demo_application/SpringDemoApplication.java
@@ -0,0 +1,13 @@
+package com.codesnippet.spring_demo_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDemoApplication.class, args);
+	}
+
+}
---
+++ spring-demo-application/src/test/java/com/codesnippet/spring_demo_application/SpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codesnippet.spring_demo_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
